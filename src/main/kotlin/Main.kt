1ff31ccdcaeddf6dacb624542c02f12a9773b1f9
LENOVO <test23062000@gmail.com>
2023-11-29 17:56:02 +0300

    dzz

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,12 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val store = Store()
-        val user = User("Гари", "Алея 15").apply {
-            balance = 10000
-        }
-        val scanner = Scanner(System.`in`)
-        println("Продукты:")
-        println("----------------------------------------")
-
+    val user = User("Гари", "Алея 15").apply {
+        balance = 10000
+    }
+    val scanner = Scanner(System.`in`)
+    println("Продукты:")
+    println("----------------------------------------")
     with(store) {
         store()
         println("----------------------------------------")
