05c22ebb3728153c0a80e4c2acbac9a5f5d09a14
LENOVO <test23062000@gmail.com>
2023-11-29 17:55:04 +0300

    dz

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,14 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val store = Store()
-    with(store) {
         val user = User("Гари", "Алея 15").apply {
             balance = 10000
         }
         val scanner = Scanner(System.`in`)
+        println("Продукты:")
+        println("----------------------------------------")
+
+    with(store) {
         store()
         println("----------------------------------------")
         print("Введите индекс товара: ")
